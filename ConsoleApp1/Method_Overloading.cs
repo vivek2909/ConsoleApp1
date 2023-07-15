0a0330bb668679564457b170d9c57c67e6712623
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-15 17:34:21 +0530

    done

---
+++ ConsoleApp1/Method_Overloading.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    public class Method_Overloading
+    {
+        static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+        static double PlusMethod(double x, double y) 
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            int myNum1 = PlusMethod(4, 6);
+            double myNum2 = PlusMethod(2.56, 7.34);
+            Console.WriteLine("Int : " +myNum1);
+            Console.WriteLine("Double : " + myNum2);
+        }
+    }
+}
