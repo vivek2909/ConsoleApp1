a7fa57dc1d7ffbb569e591459a6eee5a4f2029e0
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-17 10:22:38 +0530

    done

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack_Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stack myStack = new Stack();
+
+            myStack.Push("1st Element");
+            myStack.Push("2nd Element");
+            myStack.Push("3rd Element");
+            myStack.Push("4th Element");
+            myStack.Push("5th Element ");
+            // myStack.Pop("4th Element");
+
+
+            Console.Write("Total Numbers of elements in the stacks are : ");
+            Console.WriteLine(myStack.Count);
+
+            Console.Write("Element at top is : " + myStack.Peek());
+
+            Console.WriteLine("element :", myStack.Pop());
+            Console.Write("after at top is : " + myStack.Peek());
+
+            Console.WriteLine(myStack.Count);
+            Console.ReadLine();
+
+        }
+    }
+}
