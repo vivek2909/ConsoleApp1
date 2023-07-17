56ca622a3ab2bfced06c49248757306eda23b01b
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-17 10:36:41 +0530

    done

---
+++ ConsoleApp1/Lambda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public static class Lambda
+{
+    public static void Main()
+    {
+        List<int> list = new List<int>() { 31, 32, 33, 34, 35, 36 };
+        List<int> evenNumbers = list.FindAll(x => (x % 2) == 0);
+
+        foreach (var num in evenNumbers)
+        {
+            Console.Write("{0} ", num);
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.Read();
+    }
+}
