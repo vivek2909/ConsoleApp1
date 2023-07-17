d72b5a8fd6023feeed80aa2d63a2e5ec7460f632
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-17 10:10:53 +0530

    done

---
+++ ConsoleApp1/Stopwatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            Console.WriteLine("Press Enter to start the Stopwatch");
+            Console.ReadLine();
+
+            stopwatch.Start();
+
+            Console.WriteLine("Stopwatch is running. Please ENTER to stop.");
+            Console.ReadLine();
+
+            stopwatch.Stop();
+
+            TimeSpan elapsed = stopwatch.Elapsed;
+            Console.WriteLine("Elapsed time : " + elapsed);
+
+            Console.WriteLine("Press ENTER to Exit.");
+            Console.ReadLine();
+        }
+    }
+}
