b14894a95e1c4191cada7a257389aec15edc9729
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-17 10:40:02 +0530

    done

---
+++ ConsoleApp1/Diamond.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Diamond
+{
+    interface A
+    {
+        void Loan();
+    }
+    interface B
+    {
+        void Loan();
+    }
+    public class Mother : A
+    {
+        public void Loan()
+        {
+            Console.WriteLine("Loan taken by A.");
+        }
+    }
+    public class Father : B
+    {
+        public void Loan()
+        {
+            Console.WriteLine("Loan taken by B");
+        }
+    }
+    public class Child : A, B
+    {
+        void A.Loan()
+        {
+            Console.WriteLine("A loan paid by D");
+        }
+
+        void B.Loan()
+        {
+            Console.WriteLine("B loan paid by D");
+        }
+    }
+
+    public class Diamond
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+            ((A)child).Loan();
+            ((B)child).Loan();
+
+            Console.ReadLine();
+        }
+    }
+}
