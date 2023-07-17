86b98e9b849ce12203c114c37e4d3eb3714728f4
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-17 10:48:22 +0530

    done

---
+++ ConsoleApp1/String.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace String_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+            string name = "Hello";
+            string name1 = " ";
+            string name2 = "Buddy";
+            string result = name + name1 + name2;
+            Console.WriteLine(result + name1 + "My Name is Vivek I'm " + age + name1 + "years old.");
+        }
+    }
+}
