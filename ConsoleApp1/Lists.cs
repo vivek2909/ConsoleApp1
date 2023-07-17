8533dcefd4ddef5ac1eabbdaf3299c8e21a95652
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-17 10:33:29 +0530

    done

---
+++ ConsoleApp1/Lists.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var countries = new List<string>();
+            countries.Add("India");
+            countries.Add("Asia");
+            countries.Add("Europe");
+            countries.Add("Africa");
+            foreach (var country in countries)
+            {
+                Console.WriteLine(country);
+
+            }
+        }
+    }
+}
