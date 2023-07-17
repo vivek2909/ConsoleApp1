dbc32f602d51a1b167be16cbb02f54b48c52e9d0
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-17 10:18:22 +0530

    done

---
+++ ConsoleApp1/String_Builder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace String_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Hello");
+            sb.Append(" ");
+            sb.Append("Buddy");
+            string output = sb.ToString();
+            Console.WriteLine(output + " My Name is Vivek I'm " + age + " years old.");
+        }
+    }
+}
