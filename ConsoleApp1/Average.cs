47d6b25ea196f2f64185ce9e0d9b20fe08d9f39d
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-27 16:58:05 +0530

    done

---
+++ ConsoleApp1/Average.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Average
+    {
+        public static void Main(string[] args)
+        {
+            double number1, number2, number3, number4;
+            Console.WriteLine("Enter the First Number = ");
+            number1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number = ");
+            number2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Third Number = ");
+            number3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Fourth Number = ");
+            number4 = Convert.ToDouble(Console.ReadLine());
+            double result = (number1 + number2 + number3 + number4) / 4;
+            Console.WriteLine($"The Average of {number1},{number2},{number3},{number4} is = "+result);
+        }
+    }
+}
