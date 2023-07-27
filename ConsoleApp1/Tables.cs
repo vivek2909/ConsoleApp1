f8879399094357c176a309f7ea68b3590100bf78
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-27 15:54:39 +0530

    done

---
+++ ConsoleApp1/Tables.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Tables
+    {
+        public static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter The Number : ");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Table of the {number} is");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(number * i);
+            }
+        }
+    }
+}
