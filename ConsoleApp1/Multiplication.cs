388a3dbcf6800dc29a1a08f63456eca6d16d9a8d
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-27 14:58:49 +0530

    done

---
+++ ConsoleApp1/Multiplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Multiplication
+    {
+        public static void Main()
+        {
+            int number1,number2,number3,temp;
+            Console.WriteLine("Enter First Number : ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number : ");
+            number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Third Number : ");
+            number3 = int.Parse(Console.ReadLine());
+            temp = (number1 * number2 * number3);
+            Console.WriteLine($"{number1}*,{number2}*,{number3} = " + temp);
+        }
+    }
+}
