9b902eeb48745d76751d1589af9118647cc8f547
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-27 12:33:03 +0530

    done

---
+++ ConsoleApp1/Hello.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Hello
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Vivek Rokade");
+        }
+    }
+}
