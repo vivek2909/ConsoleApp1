fc84f8b03bc8aaf5c14802d207bce3903c23c087
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-07-27 15:34:56 +0530

    done

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int number1, number2;
+            Console.Write("First Number = ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("Second Number = ");
+            number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{number1} + {number2} = "+(number1 + number2));
+            Console.WriteLine($"{number1} - {number2} = "+(number1 - number2));
+            Console.WriteLine($"{number1} * {number2} = "+(number1 * number2));
+            Console.WriteLine($"{number1} / {number2} = "+(number1 / number2));
+            Console.WriteLine($"{number1} Mod {number2} = "+(number1 % number2));
+        }
+    }
+}
