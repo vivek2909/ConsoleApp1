ec067f581e76acc1cab7385d71c8115e41264cb3
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-08-01 15:08:52 +0530

    Added Fibonacci Program

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Car
+    {
+        private string name = "Prasad";
+            public static void Main()
+
+        {
+            string name = "paddy";
+            Console.WriteLine(name);
+
+            Car obj = new Car();
+            Console.WriteLine(obj.name);
+        }
+        //string carobj = "Tyre";
+        //Console.WriteLine(carobj);
+    }
+}
---
+++ ConsoleApp1/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Fibonacci
+    {
+        public static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3;
+            Console.Write(n1 + " " + n2);
+            for(int i = 2; i < 10; i++) 
+            {
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                Console.Write(" " +n3);
+            }
+            Console.WriteLine();
+        }
+    }
+}
