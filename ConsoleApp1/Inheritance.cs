47b0f747a208e1aad8fdda0696925e5342f7c78e
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-08-01 16:19:53 +0530

    Implemented Inheritance

---
+++ ConsoleApp1/Inheritance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class MyClass1
+    {
+        public void Method1()
+        {
+            Console.WriteLine("one");
+        }
+    }
+    public class MyClass2 : MyClass1
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Two");
+        }
+    }
+    
+public class Inheritance
+{
+    public static void Main(string[] args)
+
+    {
+        MyClass1 obj1 = new MyClass1();
+        MyClass2 obj2 = new MyClass2();
+         
+        obj1.Method1();
+        obj2.Method1();
+        
+    }
+}
+    
+    
+
