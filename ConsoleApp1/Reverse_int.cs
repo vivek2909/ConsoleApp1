0be6275bf0868ba9c5f10f1cd1b759de59173e43
vivek2909 <135337367+vivek2909@users.noreply.github.com>
2023-08-19 17:54:57 +0530

    Reverse integer

---
+++ ConsoleApp1/Reverse_int.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Reverse_int
+    {
+        static void Main(string[] args)
+        {
+            int num = 56789;
+            int reversed = ReverseInteger(num);
+            Console.WriteLine("Reversed Integer :" + reversed);
+        }
+        static int ReverseInteger(int num)
+        {
+            int reversed = 0; 
+            while (num != 0)
+            {
+                int digit = num % 10;
+                reversed = reversed * 10 + digit;
+                num /= 10;
+            }
+            return reversed;
+        }
+    }
+}
